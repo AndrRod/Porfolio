1c8518497a9b4416f5e8361c1bc28937e7776e2c
Andres <rodrigueza.federacion@gmail.com>
2021-10-31 20:19:25 -0300

    m

--- Portafolio/settings.py
+++ Portafolio/settings.py
@@ -234,13 +234,24 @@ TEMPLATES = [
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Aca agregamos postgresql
+        # 'NAME': 'MultipleChoice',
+        'USER': 'postgres',
+        'PASSWORD': 'Lgante03',
+        'HOST': '127.0.0.1',
+        'DATABASE_PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
