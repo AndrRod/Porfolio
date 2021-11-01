a41f6950e816d8a52692185521fb99c735ab83e5
Andres <rodrigueza.federacion@gmail.com>
2021-11-01 12:46:00 -0300

    m

--- Portafolio/urls.py
+++ Portafolio/urls.py
@@ -26,4 +26,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name= 'home')
 
-]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
