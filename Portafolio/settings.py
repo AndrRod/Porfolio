1310504cd974fa3edd10eb469ea27b9670453131
Andres <rodrigueza.federacion@gmail.com>
2021-11-01 13:45:22 -0300

    m

--- Portafolio/settings.py
+++ Portafolio/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-g1okie^%-_gzb#idh#gxlw*wpy^9_nxbj=s-twzvyu5#4%i2u2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
