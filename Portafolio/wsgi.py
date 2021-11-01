fd716dcae2beddc6fa52378848400b96879bcb1f
Andres <rodrigueza.federacion@gmail.com>
2021-11-01 12:29:37 -0300

    m

--- Portafolio/wsgi.py
+++ Portafolio/wsgi.py
@@ -15,7 +15,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Portafolio.settings')
 
 # application = get_wsgi_application()  
 
-
-
 application = get_wsgi_application()
 
+from whitenoise.django import DjangoWhiteNoise  
+application = DjangoWhiteNoise(application)
