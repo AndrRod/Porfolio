26477c0fb1491044a80d1679b1f4cb3d4a6f5839
Andres <rodrigueza.federacion@gmail.com>
2021-11-01 02:31:39 -0300

    m

--- Portafolio/urls.py
+++ Portafolio/urls.py
@@ -24,6 +24,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name= 'home')
+    path('/', home, name= 'home')
 
 ]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
